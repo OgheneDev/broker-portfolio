bd2cc9cae78e2c69f579a9adbcf7998a8d7612f6
OgheneDev <emmanueloghene72@gmail.com>
2024-10-19 10:11:43 +0100

    omo

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import phone from '../assets/images/phone.svg'
 import email from '../assets/images/email.svg'
 import help from '../assets/images/help.png'

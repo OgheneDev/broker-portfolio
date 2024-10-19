524b76a17470e059fc1da33c6dbfbc22396a8c1c
OgheneDev <emmanueloghene72@gmail.com>
2024-10-19 10:09:43 +0100

    fix link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 import fb from '../assets/images/facebook.svg'
 import tw from '../assets/images/twitter.svg'
